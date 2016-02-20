8681d42f3fef37144afdff1ffc61bd1f84f8ffcc
SARANG <sarang24s@gmail.com>
2016-02-20 21:27:21 +0530

    Chat_sessions_file

--- duration_between_succ_replies.py
+++ duration_between_succ_replies.py
@@ -1,38 +1,62 @@
 import os
 from datetime import datetime
+from datetime import timedelta
 f = open('time.txt', 'r')
+z3 = open('chat.txt', 'r')
 prev=0
 date1=0
-for line in f:
+
+z = open('turn_duration.txt', 'w')
+z1 = open('response_time.txt', 'w')
+z4 = open('chat_turn.txt', 'w')
+z5 = open('chat_session.txt', 'w')
+
+for line,line1 in zip(f,z3):
 	
 	a = line.index(":::")
 	p=line[:a]
 	line = line[a:]
-	b = line.index(",")
-	q=line[:b].replace(":::","")
-	r =line[b:].replace("," , "")
-	meridian = r[1:].index(" ")
-	Time = r[:meridian+1]
-	t = r[meridian+1:]
-	if t.find("PM")!=-1:
-		hour =Time.index(":")
-		n= str(int(Time[:hour].replace(":","")) + 12)
-		print p + " " +q + " " +n +" "+Time[hour+1:]
-
-	date2 = datetime.strptime(q +" "+ n +" " +Time[hour+1:]+" 00",'%m/%d/%Y %H %M %S')
-
-	if p!=prev:
-		z = open('time_intervel.txt', 'w')
-		succ = date2-date1
-		z.write(succ)
-
-
-	date1=date2
-	prev=p
+	
+	date_session=datetime.strptime(line,"::: %m/%d/%Y, %I:%M %p\n")
 
 
 
-		
+	if prev==0:
+		date1 = datetime.strptime(line,"::: %m/%d/%Y, %I:%M %p\n")
+		prev=1
+		prev_turn=p
+		prev_date_session=date_session
+
+	if prev==1 and p==prev_turn:
+		date3 = datetime.strptime(line,"::: %m/%d/%Y, %I:%M %p\n")
+	else:
+		date2 = datetime.strptime(line,"::: %m/%d/%Y, %I:%M %p\n")
+		delta= date3-date1
+
+		delta1 = date2-date3
+
+		z1.write(str(delta1) + "\n")
+		#print delta
+		prev=1
+		z4.write("\n------------------\n")
+		z.write( str(prev_turn)+  " " + str(delta) + "\n")
+		prev_turn=p
+		date1 = datetime.strptime(line,"::: %m/%d/%Y, %I:%M %p\n")
+		date3=date1
+
+	z4.write(line1)
+	
+
+	delta_session = date_session - prev_date_session
+
+	#t = datetime.strptime("02:00:00","%H:%M:%S")
+
+	if(delta_session > timedelta(hours=2)):
+		prev_date_session=date_session
+		z5.write("\n--------\n\n")
+
+	z5.write(line1)
+
 
 		
 		
